1606850cc0a8ab4b9bedd81c90092ad7ae5eb212
liupeng <784606255@qq.com>
2016-03-27 16:17:46 +0800

    test

--- src/main/java/cn/hist/security/service/domain/MyUserDetail.java
+++ src/main/java/cn/hist/security/service/domain/MyUserDetail.java
@@ -13,19 +13,19 @@ public class MyUserDetail implements UserDetails{
 	
 	/**
 	 * 
+	 * 
+	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
 	private String username;
 	private String password;
-	
-	
 
 	public void setUsername(String username) {
 		this.username = username;
 		
 	}
-
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -38,11 +38,8 @@ public class MyUserDetail implements UserDetails{
 
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		
-		
 		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_ABCS"); 
 		return Collections.singletonList(simpleGrantedAuthority);
-		 
 	}
 
 	
